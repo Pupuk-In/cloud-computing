be329921841dfefef2cecb585df1e2623e6a6663
Phr <farrelibrahim04@gmail.com>
2023-05-19 20:57:03 +0700

    plsplsplsplsplspls

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ RUN cd /app && \
 
 RUN chown -R www-data: /app
 
-CMD sh /app/docker/startup.sh
+CMD sh /app/startup.sh
--- startup.sh
+++ startup.sh
@@ -6,7 +6,6 @@ php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
-cd ..
 cd src
 composer install
 php artisan migrate
