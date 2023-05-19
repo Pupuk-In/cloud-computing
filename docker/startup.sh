664e0f783d1bfef134685cb3ece32442259e7401
Phr <farrelibrahim04@gmail.com>
2023-05-19 19:17:50 +0700

    Bismillah

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,6 +6,8 @@ php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
+cd /app/src
+composer install
 php artisan key:generate && php artisan migrate:refresh --seed
 php artisan passport:install
 
