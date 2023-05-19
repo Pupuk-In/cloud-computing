5f8ded1b7ff7ffa181e5b41462a9e149698b20e5
Phr <farrelibrahim04@gmail.com>
2023-05-19 20:03:04 +0700

    plsplspls

--- docker/startup.sh
+++ docker/startup.sh
@@ -6,9 +6,11 @@ php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
-cd /app/src
-composer install
-php artisan key:generate && php artisan migrate:refresh --seed
+php artisan migrate
+php artisan key:generate 
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
 php artisan passport:install
 
 nginx
