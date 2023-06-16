a2881d9678649d6732e9871cae04d1ade75613f6
Phr <farrelibrahim04@gmail.com>
2023-06-16 21:22:40 +0700

    a little bit more fix

--- src/app/Http/Controllers/Api/SearchController.php
+++ src/app/Http/Controllers/Api/SearchController.php
@@ -82,7 +82,34 @@ class SearchController extends Controller
         if($request->sort){
             $sortColumn = $request->sort;
             $sortOrder = $request->order === 'desc' ? 'desc' : 'asc';
-            $sortedItems = $itemQuery->orderBy($sortColumn, $sortOrder)->get();
+            if($request->sort != 'relevance'){
+                $sortedItems = $itemQuery->orderBy($sortColumn, $sortOrder)->get();
+            } else {
+                $itemSort = $itemQuery->select('items.*')->get();
+
+                // $sortedItems = $itemQuery->orderBy("created_at", "desc")->paginate(10);
+                foreach($responseData as $key => $value){
+                    foreach($itemSort as $itemed){
+                        if($itemed->id == $value['id']){
+                            $itemed['relevance'] = $value['relevance'];
+                        }
+                    }
+                }
+
+                $sortedItems = collect();
+
+                $itemSort->sort(function ($item1, $item2) use ($sortedItems) {
+                    // Compare the "relevance" values of $item1 and $item2
+                    if ($item1->relevance === $item2->relevance) {
+                        return 0;  // The "relevance" values are equal
+                    }
+
+                    // Sort in descending order by comparing "relevance" values
+                    return ($item1->relevance > $item2->relevance) ? -1 : 1;
+                })->each(function ($item) use ($sortedItems) {
+                    $sortedItems->push($item);
+                });
+            }
         } else {
             $itemSort = $itemQuery->select('items.*')->get();
 
